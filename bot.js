5117d42b1c1ec58a9fcc8b7dce91766ebf56d154
marceloranngel <marcelo.rcs@icloud.com>
2020-03-13 21:30:22 -0300

    Correção Bot

--- bot.js
+++ bot.js
@@ -2,35 +2,44 @@ const Twit = require('twit');
 const config = require('./config.js');
 const tweetBot = new Twit(config);
 
-const params = {
-    q: 'Maconha',
+
+const verifiedmediaparams = {
+    q: 'Corona OR covid-19 OR coronavírus filter:verified filter:media',
     result_type: 'popular',
-    count: 70,
+    count: 20,
     lang: 'pt'
 }
 
-const nearparams = {
-    q: 'Maconha',
+
+const bolsonaroparams = {
+    q: 'from:jairbolsonaro Corona OR covid-19 OR coronavírus filter:verified',
+    result_type: 'popular',
+    count: 50,
+    lang: 'pt'
+}
+
+const publicoparams = {
+    q: 'Corona OR covid-19 OR coronavírus ?',
     result_type: 'recent',
-    count: 30,
+    count: 90,
     lang: 'pt'
 }
 
 
-tweetBot.get('search/tweets', params,  (err, data, res) => {
+tweetBot.get('search/tweets', verifiedmediaparams,  (err, data, res) => {
     if(!err){
         for(let i=0; i < data.statuses.length; i++){
             let tweetID = {id: data.statuses[i].id_str}
             tweetBot.post('statuses/retweet', tweetID, (err, res) => {
                 if(!err){
-                    console.log(`Retweet successful` + i)
+                    console.log(`Conta Verificada Retwwetado com Sucesso nº ` + i)
                 }else{
                     console.log(err.message)
                 }
             })
             tweetBot.post('favorites/create', tweetID, (err, res) => {
                 if(!err){
-                    console.log(`Favorite successful` + i)
+                    console.log(`Conta Verificada Curtida com Sucesso nº ` + i)
                 }else{
                     console.log(err.message)
                 }
@@ -41,20 +50,37 @@ tweetBot.get('search/tweets', params,  (err, data, res) => {
     }
 })
 
-tweetBot.get('search/tweets', nearparams,  (err, data, res) => {
+tweetBot.get('search/tweets', bolsonaroparams,  (err, data, res) => {
     if(!err){
         for(let i=0; i < data.statuses.length; i++){
             let tweetID = {id: data.statuses[i].id_str}
             tweetBot.post('statuses/retweet', tweetID, (err, res) => {
                 if(!err){
-                    console.log(`Retweet Recent successful` + i)
+                    console.log(`Bolsonaro Retweetado com Sucesso nº ` + i)
                 }else{
                     console.log(err.message)
                 }
             })
             tweetBot.post('favorites/create', tweetID, (err, res) => {
                 if(!err){
-                    console.log(`Favorite Recent successful` + i)
+                    console.log(`Bolsonaro Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', publicoparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Publico Curtido com Sucesso nº ` + i)
                 }else{
                     console.log(err.message)
                 }
