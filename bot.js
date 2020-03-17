9eb96c90cf664a384d50f201c89752f980339410
marceloranngel <marcelo.rcs@icloud.com>
2020-03-17 12:04:26 -0300

    CoAdicionando mais contas no filtro + URL Filter

--- bot.js
+++ bot.js
@@ -5,52 +5,81 @@ const tweetBot = new Twit(config);
 
 const verifiedmediaparams = {
     q: 'Corona OR covid-19 OR coronavírus filter:verified filter:media',
-    result_type: 'popular',
-    count: 20,
-    lang: 'pt'
-}
-
-const verifiedrecentparams = {
-    q: 'Corona OR covid-19 OR coronavírus filter:verified filter:media',
-    result_type: 'recent',
+    result_type: 'mixed',
     count: 20,
     lang: 'pt'
 }
 
 const bolsonaroparams = {
     q: 'from:jairbolsonaro Corona OR covid-19 OR coronavírus filter:verified',
-    result_type: 'popular',
-    count: 50,
-    lang: 'pt'
-}
-
-const bolsonarorecentparams = {
-    q: 'from:jairbolsonaro Corona OR covid-19 OR coronavírus filter:verified',
-    result_type: 'recent',
+    result_type: 'mixed',
     count: 50,
     lang: 'pt'
 }
 
 const publicoparams = {
     q: 'Corona OR covid-19 OR coronavírus',
-    result_type: 'recent',
-    count: 90,
-    lang: 'pt'
-}
-
-const minsauderecentparams = {
-    q: 'from:minsaude Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' , 
-    result_type: 'recent',
-    count: 20,
+    result_type: 'mixed',
+    count: 100,
     lang: 'pt'
 }
 
 const minsaudeparams = {
     q: 'from:minsaude Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
-    result_type: 'popular',
-    count: 20,
+    result_type: 'mixed',
+    count: 60,
     lang: 'pt'
 }
+
+const mineconparams = {
+    q: 'from:mineconomia Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
+    result_type: 'mixed',
+    count: 60,
+    lang: 'pt'
+}
+
+const mcticparams = {
+    q: 'from:mctic Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
+    result_type: 'mixed',
+    count: 60,
+    lang: 'pt'
+}
+
+const secomvcparams = {
+    q: 'from:secomvc Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
+    result_type: 'mixed',
+    count: 60,
+    lang: 'pt'
+}
+
+const tvbrasilgovparams = {
+    q: 'from:tvbrasilgov Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
+    result_type: 'mixed',
+    count: 60,
+    lang: 'pt'
+}
+
+const govbrparams = {
+    q: 'from:govbr Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
+    result_type: 'mixed',
+    count: 60,
+    lang: 'pt'
+}
+
+const GloboNewsparams = {
+    q: 'from:GloboNews Corona OR covid-19 OR coronavírus OR #Coronavírus filter:verified' ,
+    result_type: 'mixed',
+    count: 60,
+    lang: 'pt'
+}
+
+const urlgoneparams = {
+    q: 'Corona OR covid-19 OR coronavírus OR #Coronavírus url:g1' ,
+    result_type: 'mixed',
+    count: 100,
+    lang: 'pt'
+}
+
 
 tweetBot.get('search/tweets', verifiedmediaparams,  (err, data, res) => {
     if(!err){
@@ -76,55 +105,7 @@ tweetBot.get('search/tweets', verifiedmediaparams,  (err, data, res) => {
     }
 })
 
-tweetBot.get('search/tweets', verifiedrecentparams,  (err, data, res) => {
-    if(!err){
-        for(let i=0; i < data.statuses.length; i++){
-            let tweetID = {id: data.statuses[i].id_str}
-            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
-                if(!err){
-                    console.log(`Conta Verificada Recent Retwwetado com Sucesso nº ` + (i + 1))
-                }else{
-                    console.log(err.message)
-                }
-            })
-            tweetBot.post('favorites/create', tweetID, (err, res) => {
-                if(!err){
-                    console.log(`Conta Verificada Curtida com Sucesso nº ` + i)
-                }else{
-                    console.log(err.message)
-                }
-            })
-        }
-    }else{
-        console.log(err)
-    }
-})
-
 tweetBot.get('search/tweets', bolsonaroparams,  (err, data, res) => {
-    if(!err){
-        for(let i=0; i < data.statuses.length; i++){
-            let tweetID = {id: data.statuses[i].id_str}
-            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
-                if(!err){
-                    console.log(`Bolsonaro Retweetado com Sucesso nº ` + i)
-                }else{
-                    console.log(err.message)
-                }
-            })
-            tweetBot.post('favorites/create', tweetID, (err, res) => {
-                if(!err){
-                    console.log(`Bolsonaro Curtido com Sucesso nº ` + i)
-                }else{
-                    console.log(err.message)
-                }
-            })
-        }
-    }else{
-        console.log(err)
-    }
-})
-
-tweetBot.get('search/tweets', bolsonarorecentparams,  (err, data, res) => {
     if(!err){
         for(let i=0; i < data.statuses.length; i++){
             let tweetID = {id: data.statuses[i].id_str}
@@ -172,30 +153,6 @@ tweetBot.get('search/tweets', minsaudeparams,  (err, data, res) => {
     }
 })
 
-tweetBot.get('search/tweets', minsauderecentparams,  (err, data, res) => {
-    if(!err){
-        for(let i=0; i < data.statuses.length; i++){
-            let tweetID = {id: data.statuses[i].id_str}
-            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
-                if(!err){
-                    console.log(`Ministerio da Saude Recente Retweetado com Sucesso nº ` + i)
-                }else{
-                    console.log(err.message)
-                }
-            })
-            tweetBot.post('favorites/create', tweetID, (err, res) => {
-                if(!err){
-                    console.log(`Ministerio da Saude Recente Curtido com Sucesso nº ` + i)
-                }else{
-                    console.log(err.message)
-                }
-            })
-        }
-    }else{
-        console.log(err)
-    }
-})
-
 tweetBot.get('search/tweets', publicoparams,  (err, data, res) => {
     if(!err){
         for(let i=0; i < data.statuses.length; i++){
@@ -213,3 +170,163 @@ tweetBot.get('search/tweets', publicoparams,  (err, data, res) => {
     }
 })
 
+tweetBot.get('search/tweets', mineconparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Ministerio da Economia Retweetado com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Ministerio da Economia Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', mcticparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Ministerio da Ciencia Retweetado com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Ministerio da Ciencia Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', secomvcparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Secretaria de Comunicação Retweetado com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Secretaria de Comunicação Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', tvbrasilgovparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`TV Brasil Retweetado com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`TV Brasil Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', govbrparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Governo Brasileiro Retweetado com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Governo Brasileiro Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', GloboNewsparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('statuses/retweet', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Globo News Retweetado com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`Globo News Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
+
+tweetBot.get('search/tweets', urlgoneparams,  (err, data, res) => {
+    if(!err){
+        for(let i=0; i < data.statuses.length; i++){
+            let tweetID = {id: data.statuses[i].id_str}
+            tweetBot.post('favorites/create', tweetID, (err, res) => {
+                if(!err){
+                    console.log(`URL G1 Curtido com Sucesso nº ` + i)
+                }else{
+                    console.log(err.message)
+                }
+            })
+        }
+    }else{
+        console.log(err)
+    }
+})
